25231325771c0beca1f581473367fda250b0983c
Ruben Jenster <rjenster@gmail.com>
2014-03-05 15:12:42 +0100

    Fixed several compiler warnings.

--- test/trace.h
+++ test/trace.h
@@ -8,6 +8,7 @@
 
 #ifndef TRACE
 
+#define TRACE_INIT
 #define TRACE_BEGIN
 #define TRACE_BEGIN_FMT
 #define TRACE_END
@@ -22,19 +23,13 @@
 #define CLOCKS_PER_SEC 1000000
 #endif
 
-static struct timeval _TRACE_TIME_START;
-static struct timeval _TRACE_TIME_END;
-static double _TRACE_TIME_DIFF;
-
-static clock_t _TRACE_CLOCK_START;
-static clock_t _TRACE_CLOCK_END;
-static double _TRACE_CLOCK_DIFF;
-
-static const char *_TRACE_BEGIN_FMT =
-	"~~> TRACE BEGIN";
-static const char *_TRACE_END_FMT =
-	"<~~ TRACE END (real: %.4f ms, cpu: %.4f ms)\n";
-
+#define TRACE_INIT \
+		struct timeval _TRACE_TIME_START; \
+		struct timeval _TRACE_TIME_END; \
+		double _TRACE_TIME_DIFF; \
+		clock_t _TRACE_CLOCK_START; \
+		clock_t _TRACE_CLOCK_END; \
+		double _TRACE_CLOCK_DIFF;
 /*
  * Leaving the variadic args off only works with LLVM/GCC
  * see http://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html
@@ -43,13 +38,13 @@ static const char *_TRACE_END_FMT =
 #define TRACE_BEGIN_FMT(fmt, ...) \
 	gettimeofday(&_TRACE_TIME_START, NULL); \
 	_TRACE_CLOCK_START = clock(); \
-	puts(_TRACE_BEGIN_FMT); \
+	puts("~~> TRACE BEGIN"); \
 	printf("~~? " fmt, __VA_ARGS__);
 
 #define TRACE_BEGIN(message) \
 	gettimeofday(&_TRACE_TIME_START, NULL); \
 	_TRACE_CLOCK_START = clock(); \
-	puts(_TRACE_BEGIN_FMT); \
+	puts("~~> TRACE BEGIN"); \
 	puts("~~? " message);
 
 #define TRACE_END \
@@ -58,7 +53,7 @@ static const char *_TRACE_END_FMT =
 	_TRACE_TIME_DIFF += (_TRACE_TIME_END.tv_usec - _TRACE_TIME_START.tv_usec) / 1000.0; \
 	_TRACE_CLOCK_END = clock(); \
 	_TRACE_CLOCK_DIFF = (double)(_TRACE_CLOCK_END - _TRACE_CLOCK_START) / CLOCKS_PER_SEC * 1000.0; \
-	printf(_TRACE_END_FMT, _TRACE_TIME_DIFF, _TRACE_CLOCK_DIFF);
+	printf("<~~ TRACE END (real: %.4f ms, cpu: %.4f ms)\n", _TRACE_TIME_DIFF, _TRACE_CLOCK_DIFF);
 
 
 #endif
