3aeb4ccec74a9bf2ef6f1998990d6664d5400745
Ruben Jenster <rjenster@gmail.com>
2014-03-05 15:13:14 +0100

    Added more strict CFLAGS.

--- module.mk
+++ module.mk
@@ -1,4 +1,7 @@
 MODULES := libcx-list libcx-workqueue libcx-socket-unix
 
-CFLAGS += --coverage -I$(BASE_DIR) -DTRACE
+CFLAGS += --coverage -I$(BASE_DIR) -DTRACE \
+	-Wall -Wextra -Werror -pedantic \
+	-Wno-error=unused-parameter -Wno-error=unused-function
+	
 LDFLAGS += --coverage
