a63e88919f65b9c3cca39b73b4c744c70201b235
Ruben Jenster <rjenster@gmail.com>
2014-03-07 10:33:47 +0100

    Update submodules. No compilation error on unused variables.

--- module.mk
+++ module.mk
@@ -1,8 +1,10 @@
-MODULES := libcx-base libcx-list libcx-socket-unix libcx-workqueue libcx-buffer
+MODULES := libcx-base libcx-list libcx-socket-unix \
+	libcx-workqueue libcx-buffer libcx-umtp
 
 CFLAGS += --coverage -I$(BASE_DIR) -DTRACE \
 	-Wall -Wextra -Werror -pedantic \
 	-Wno-error=unused-parameter -Wno-error=unused-function \
+	-Wno-error=unused-variable \
 	-gdwarf-2 -g -O0 -fno-inline
 	
 LDFLAGS += --coverage
