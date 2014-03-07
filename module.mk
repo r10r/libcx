a51470430d8c6d82d5483095e2e1726647305226
Ruben Jenster <rjenster@gmail.com>
2014-03-07 09:18:31 +0100

    Added debug CFLAGS flags.

--- module.mk
+++ module.mk
@@ -2,6 +2,7 @@ MODULES := libcx-base libcx-list libcx-socket-unix libcx-workqueue
 
 CFLAGS += --coverage -I$(BASE_DIR) -DTRACE \
 	-Wall -Wextra -Werror -pedantic \
-	-Wno-error=unused-parameter -Wno-error=unused-function
+	-Wno-error=unused-parameter -Wno-error=unused-function \
+	-gdwarf-2 -g -O0 -fno-inline
 	
 LDFLAGS += --coverage
