ab245ee6406467a8117852213e9f8ba893f3f238
Ruben Jenster <rjenster@gmail.com>
2014-03-10 11:31:47 +0100

    Formatted sources.

--- xmalloc.c
+++ xmalloc.c
@@ -1,4 +1,4 @@
- /* do not replace malloc/realloc/free with macros here */
+/* do not replace malloc/realloc/free with macros here */
 #define _XMALLOC_DISABLED
 #include "xmalloc.h"
 
@@ -6,8 +6,9 @@ void *
 d_xmalloc(size_t size, const char *func, const char *file, int line)
 {
 	void *ptr = malloc(size);
+
 	printf("MALLOC[%p size %ld] - (%s):%s:%d\n",
-			ptr, size, func, file, line);
+	       ptr, size, func, file, line);
 	return ptr;
 }
 
@@ -15,8 +16,9 @@ void *
 d_xrealloc(void *ptr, size_t size, const char *func, const char *file, int line)
 {
 	void* new_ptr = realloc(ptr, size);
+
 	printf("REALLOC[%p -> %p (new size %ld) - (%s):%s:%d\n",
-			ptr, new_ptr, size, func, file, line);
+	       ptr, new_ptr, size, func, file, line);
 	return new_ptr;
 }
 
