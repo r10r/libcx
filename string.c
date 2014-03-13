16a54fd15017e41e98d0395f8f6c04da9fcac5e5
Ruben Jenster <rjenster@gmail.com>
2014-03-13 13:52:37 +0100

    Implemented String_append_stream and String_write methods. Fixed several compiler warnings/errors (mainly implicit type conversions).

--- string.c
+++ string.c
@@ -3,18 +3,20 @@
 /* based on the redis sds.c (simple dynamic string) */
 
 String
-String_init(const void *value, size_t length)
+String_init(const void *value, unsigned int length)
 {
+	// TODO check if maximum length is exceeded
 	struct string_header_t *hdr;
 
-	if(value)
+	if (value)
 		hdr =  malloc(String_size(length));
 	else
 		hdr =  calloc(String_size(length), 1);
 
-	if(hdr == NULL) return NULL; // OOM
+	if (hdr == NULL)
+		return NULL;         // OOM
 
-	hdr->space = length;
+	hdr->available = length;
 	hdr->unused = 0;
 
 	if (length > 0 && value)
@@ -28,40 +30,47 @@ String_init(const void *value, size_t length)
 String
 String_new(const char *value)
 {
-	size_t length = (value == NULL) ? 0 : strlen(value);
+	// asssert strlen(value) < UINT32_MAX
+	unsigned int length = (value == NULL) ? 0 : (unsigned int)strlen(value);
+
 	return String_init(value, length);
 }
 
 void
 String_free(String s)
 {
-	if (s == NULL) return;
+	if (s == NULL)
+		return;
 	free(String_header(s));
 }
 
 String
-String_grow(String s, size_t required_space)
+String_grow(String s, unsigned int required_size)
 {
-  struct string_header_t *current_hdr, *new_hdr;
-  size_t unused_space = String_unused(s);
-  size_t current_length, new_length;
-  if(unused_space >= required_space) return s;
+	// TODO check if maximum length is exceeded
+	struct string_header_t *current_hdr, *new_hdr;
+	unsigned int unused_space = String_unused(s);
+	unsigned int current_length, new_length;
 
-  current_length = String_length(s);
-  current_hdr = String_header(s);
-  new_length = current_length + required_space;
+	if (unused_space >= required_size)
+		return s;
+
+	current_length = String_length(s);
+	current_hdr = String_header(s);
+	new_length = current_length + required_size;
 //  if (new_length < STRING_MAX_PREALLOC)
-//  	new_length *= 2;
+//      new_length *= 2;
 //  else
-//  	new_length += STRING_MAX_PREALLOC;
+//      new_length += STRING_MAX_PREALLOC;
 
-  new_hdr = realloc(current_hdr, String_size(new_length));
-  if (new_hdr == NULL) return NULL;
-  new_hdr->space = new_length;
-  new_hdr->unused = new_length - current_length;
-  new_hdr->buf[new_length]  = '\0';
+	new_hdr = realloc(current_hdr, String_size(new_length));
+	if (new_hdr == NULL)
+		return NULL;
+	new_hdr->available = new_length;
+	new_hdr->unused = new_length - current_length;
+	new_hdr->buf[new_length]  = '\0';
 
-  return new_hdr->buf;
+	return new_hdr->buf;
 }
 
 String
@@ -71,25 +80,53 @@ String_shrink(String s)
 	return NULL;
 }
 
+static inline String
+_append(String a, size_t a_length, const char *b, unsigned int b_length)
+{
+	String c = String_grow(a, b_length);
+
+	if (c == NULL)
+		return NULL;
+	memcpy(&c[a_length], b, b_length);
+	return c;
+}
+
+String
+String_append_array(String a, const char *b, unsigned int b_length)
+{
+	if (a == NULL)
+		return NULL;
+	if (b == NULL)
+		return a;
+	return _append(a, String_length(a), b, b_length);
+}
+
+String
+String_append_constant(String a, const char *b)
+{
+	if (a == NULL)
+		return NULL;
+	if (b == NULL)
+		return a;
+	// TODO check for string maximum length
+	return _append(a, String_length(a), b, (unsigned int)strlen(b));
+}
+
 String
 String_append(String a, String b)
 {
-	if (a == NULL) return NULL;
-	if (b == NULL) return a;
-
-	size_t a_length = String_length(a);
-
-	String c = String_grow(a, String_length(b));
-	if (c == NULL) return NULL;
-	memcpy(&c[a_length], b, String_length(b));
-
-	return c;
+	if (a == NULL)
+		return NULL;
+	if (b == NULL)
+		return a;
+	return _append(a, String_length(a), b, String_length(b));
 }
 
 Pair *
 StringPair_init(String key, String value)
 {
 	Pair *p = malloc(sizeof(Pair));
+
 	p->key = key;
 	p->value = value;
 	return p;
@@ -104,7 +141,27 @@ StringPair_new(const char *key, const char *value)
 void
 StringPair_free(Pair *p)
 {
-	String_free((String) p->key);
-	String_free((String) p->value);
+	String_free((String)p->key);
+	String_free((String)p->value);
 	free(p);
+}
+
+size_t
+String_write(String s, FILE *file)
+{
+	return fwrite(s, sizeof(char), String_length(s), file);
+}
+
+String
+String_append_stream(String s, FILE *file, unsigned int length)
+{
+	unsigned int required_size = length * sizeof(char);
+	String buf = String_grow(s, required_size);
+
+	struct string_header_t *hdr = String_header(buf);
+	/* we don't read more than required_size chars/bytes */
+	unsigned int read = (unsigned int )fread(&buf[String_length(buf)], 1, required_size, file);
+
+	hdr->unused = required_size - read;
+	return buf;
 }
--- test_string.c
+++ test_string.c
@@ -4,7 +4,7 @@
 
 NOSETUP
 
-void test_String_new()
+static void test_String_new()
 {
 	String s = String_new("foo");
 
@@ -13,7 +13,7 @@ void test_String_new()
 	String_free(s);
 }
 
-void test_String_append()
+static void test_String_append()
 {
 	String foo = String_new("foo");
 	String bar = String_new("bar");
@@ -26,7 +26,30 @@ void test_String_append()
 	String_free(bar);
 }
 
-void test_StringPair_new()
+static void test_String_append_constant()
+{
+	String foo = String_new("foo");
+	String foobar = String_append_constant(foo, "bar");
+
+	printf("%s\n", foobar);
+	TEST_ASSERT_EQUAL_INT(6, String_space(foobar));
+	TEST_ASSERT_EQUAL_STRING("foobar", foobar);
+	String_free(foobar);
+}
+
+static void test_String_append_array()
+{
+	const char f[] = "foobar";
+	String foo = String_new("foo");
+	String foobar = String_append_array(foo, &f[3], 3);
+
+	printf("%s\n", foobar);
+	TEST_ASSERT_EQUAL_INT(6, String_space(foobar));
+	TEST_ASSERT_EQUAL_STRING("foobar", foobar);
+	String_free(foobar);
+}
+
+static void test_StringPair_new()
 {
 	Pair *foobar = StringPair_new("foo", "bar");
 
@@ -35,13 +58,51 @@ void test_StringPair_new()
 	StringPair_free(foobar);
 }
 
+static void test_String_write()
+{
+	String foo = String_new("foo\n");
+
+	TEST_ASSERT_EQUAL_STRING("foo\n", foo);
+	TEST_ASSERT_EQUAL_INT(4, String_write(foo, stdout));
+	String_free(foo);
+}
+
+static void test_String_append_stream()
+{
+	char template[] = "/tmp/temp.XXXXXX";
+	int fd = mkstemp(template);
+	FILE *tmpfile = fdopen(fd, "w+");
+
+	fwrite("foo\n", 4, 1, tmpfile);
+
+	String foo = String_new(NULL);
+
+	rewind(tmpfile);
+	foo = String_append_stream(foo, tmpfile, 4);
+	TEST_ASSERT_EQUAL_STRING("foo\n", foo);
+	TEST_ASSERT_EQUAL_INT(4, String_write(foo, stdout));
+
+	rewind(tmpfile);
+	foo = String_append_stream(foo, tmpfile, 4);
+	TEST_ASSERT_EQUAL_STRING("foo\nfoo\n", foo);
+	TEST_ASSERT_EQUAL_INT(8, String_write(foo, stdout));
+
+	String_free(foo);
+	fclose(tmpfile);
+	unlink(template);
+}
+
 int main()
 {
 	TEST_BEGIN
 
 	RUN(test_String_new);
 	RUN(test_String_append);
+	RUN(test_String_append_constant);
+	RUN(test_String_append_array);
 	RUN(test_StringPair_new);
+	RUN(test_String_write);
+	RUN(test_String_append_stream);
 
 	TEST_END
 }
