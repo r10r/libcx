0308d62f86d235645e76031dabe15d23e119916d
Ruben Jenster <rjenster@gmail.com>
2014-03-17 22:46:13 +0100

    Added additional check for `container_of` macro.

--- base.h
+++ base.h
@@ -6,14 +6,14 @@
 /* http://psomas.wordpress.com/2009/07/01/weird-kernel-macros-container_of/ */
 
 /*
- * Typechecking ANSI-C conformable version of container_of
+ * Type safe ANSI-C conformable version of container_of
  * @from http://stackoverflow.com/questions/10269685/kernels-container-of-any-way-to-make-it-iso-conforming
  *
  * @ptr pointer to the member
  * @type type of the container
  * @member name of the member in the container
  *
- * -Werror=compare-distinct-pointer-types
+ * requires clang with `-Werror=compare-distinct-pointer-types` enabled
  */
 #define container_of(ptr, type, member) \
 	((type*)((char*)(ptr) - offsetof(type, member) + \
