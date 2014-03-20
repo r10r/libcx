487c835cba72b6a3dbcc8e7c30ae71f059ed5c18
Ruben Jenster <rjenster@gmail.com>
2014-03-20 13:10:02 +0100

    Added wrapper for debugging 'calloc' calls.

--- xmalloc.c
+++ xmalloc.c
@@ -13,11 +13,22 @@ d_xmalloc(size_t size, const char *func, const char *file, int line)
 }
 
 void *
+d_xcalloc(size_t count, size_t size, const char *func, const char *file, int line)
+{
+	void *ptr = calloc(count, size);
+
+	printf("CALLOC[%p count %ld, size %ld] - (%s):%s:%d\n",
+	       ptr, count, size, func, file, line);
+	return ptr;
+}
+
+
+void *
 d_xrealloc(void *ptr, size_t size, const char *func, const char *file, int line)
 {
 	void* new_ptr = realloc(ptr, size);
 
-	printf("REALLOC[%p -> %p (new size %ld) - (%s):%s:%d\n",
+	printf("REALLOC[%p -> %p] (new size %ld) - (%s):%s:%d\n",
 	       ptr, new_ptr, size, func, file, line);
 	return new_ptr;
 }
