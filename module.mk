9bd408d98a512090dafd8d45f0ac1d5c4ea8e426
Ruben Jenster <rjenster@gmail.com>
2014-03-20 08:48:02 +0100

    Implemented simple key-value `StringPair`

--- module.mk
+++ module.mk
@@ -3,7 +3,7 @@ L := $(LOCAL_DIR)
 
 #CFLAGS += -D_LIST_DISABLE_LOCKING
 
-TESTS += $(L)/test_string
+TESTS += $(L)/test_string $(L)/test_string_pair
 
 # -- executables --
 #_OBJS := 
@@ -11,8 +11,12 @@ TESTS += $(L)/test_string
 
 # -- tests -- 
 TEST_FLAGS := -Wall -w -g -I$(BASE_DIR)
-TEST_OBJS := $(BASE_DIR)/libcx-base/unity.o #$(BASE_DIR)/libcx-base/xmalloc.o 
+TEST_OBJS := $(BASE_DIR)/libcx-base/unity.o $(BASE_DIR)/libcx-base/xmalloc.o 
 
 $(L)/test_string_OBJS := $(TEST_OBJS) \
 	 $(L)/string.o $(L)/test_string.o
 $(L)/test_string_FLAGS := $(TEST_FLAGS)
+
+$(L)/test_string_pair_OBJS := $(TEST_OBJS) \
+	 $(L)/string.o $(L)/pair.o $(L)/test_string_pair.o
+$(L)/test_string_pair_FLAGS := $(TEST_FLAGS)
---
+++ pair.h
@@ -0,0 +1,21 @@
+#ifndef _STRING_PAIR_H
+#define _STRING_PAIR_H
+
+#include "string.h"
+
+typedef struct string_pair_t
+{
+	String *key;
+	String *value;
+} StringPair;
+
+StringPair *
+StringPair_init(String *key, String *value);
+
+StringPair *
+StringPair_new(const char *key, const char *value);
+
+void
+StringPair_free(StringPair *pair);
+
+#endif
