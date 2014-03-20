7e414ccf04382df91f02506be70e93448ea66d48
Ruben Jenster <rjenster@gmail.com>
2014-03-20 17:12:23 +0100

    Add method to read message from a file stream / descriptor.

--- message-parser.c
+++ message-parser.c
@@ -1,23 +1,13 @@
+#include "libcx-base/debug.h"
 #include "message.h"
 
 int
 main(int argc, char **argv)
 {
-	Message *message = Message_new(1024);
-	int status = 0;
+	Message *message = Message_new(0);
 
-	while (true)
-	{
-		message->parser_state->buffer = String_append_stream(message->parser_state->buffer, stdin, 1024);
-
-		if (feof(stdin))
-		{
-			status = Message_parse_finish(message);
-			break;
-		}
-		else
-			Message_parse(message);
-	}
-
-	return status;
+	Message_buffer_read(message, fileno(stdin), (size_t)atoi(argv[1]));
+	Message_parse_finish(message);
+	// do something with the message
+	return 0;
 }
