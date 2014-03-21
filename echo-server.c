077db3d52ba8305231300f81bf7d5f098c4e7915
Ruben Jenster <rjenster@gmail.com>
2014-03-21 12:33:56 +0100

    Implemented echo server.

--- echo-server.c
+++ echo-server.c
@@ -1,137 +1,55 @@
+#include "libcx-base/test.h"
+#include "libcx-base/base.h"
 #include "unix_server.h"
 
-#define READ_EOF 0
-#define IO_ERR -1               /* recv/send or read/write error */
+#define CONNECTION_BUFFER_LENGTH 1024
 
-static int server_fd;
-
-static void
-on_client_write(ev_loop *loop, ev_io *w, int revents)
+static ssize_t
+echo_connection_data_handler(Connection *connection)
 {
-	Worker *worker = (Worker*)ev_userdata(loop);
+	StringBuffer *buffer = (StringBuffer*)connection->data;
 
-	XFLOG("Worker[%lu] fd:%d - sending response\n", worker->id, w->fd);
-	ev_io_stop(loop, w);
-	char data[] = "foo";
-	size_t data_length = strlen(data);
-	ssize_t bytes = send(w->fd, data, data_length, 0);
-	if (bytes < 0 || (int)bytes < (int)data_length)
-		XFLOG("Worker[%lu] fd:%d - failed to send response\n", worker->id, w->fd);
-//	thread_wait(100 * 1000000); /* delay used for testing purposes */
-	close(w->fd);
-	free(w);
+	// fill buffer
+	return StringBuffer_nread(buffer, 0, connection->connection_fd, buffer->length);
 }
 
-static void
-on_client_read(ev_loop *loop, ev_io *w, int revents)
+static Connection*
+echo_connection_handler(Connection *connection, ConnectionEvent event)
 {
-	Worker *worker = (Worker*)ev_userdata(loop);
-	char buf[200];
-
-	XFLOG("Worker[%lu] fd:%d - read connection (revents:%d)\n", worker->id, w->fd, revents);
-	/*
-	 * @see man 1 read
-	 * A value of zero indicates end-of-file (except if the value of the size argument is also zero).
-	 * This is not considered an error. If you keep calling read while at end-of-file,
-	 * it will keep returning zero and doing nothing else.
-	 */
-
-	ssize_t num_read = read(w->fd, buf, 200);
-
-	if (num_read == IO_ERR)
+	switch (event)
 	{
-		XFLOG("Worker[%lu] fd:%d - failed to receive data\n", worker->id, w->fd);
-		ev_io_stop(loop, w);
-	}
-	else
+	case CONNECTION_EVENT_ACCEPTED:
+		XDBG("connection accepted");
+		connection->data = StringBuffer_new(CONNECTION_BUFFER_LENGTH);
+		break;
+	case CONNECTION_EVENT_DATA:
 	{
-		if (num_read == READ_EOF)
-		{
-			XFLOG("Worker[%lu] fd:%d - EOF\n", worker->id, w->fd);
-			ev_io *send_response_w = malloc(sizeof(ev_io));
-			ev_io_init(send_response_w, on_client_write, w->fd, EV_WRITE);
-			ev_io_start(loop, send_response_w);
-			ev_io_stop(loop, w); /* stop reading from socket */
-			free(w);
-		}
-		else
-			XFLOG("Worker[%lu] fd:%d - received data (length %lu)\n", worker->id, w->fd, num_read);
+		StringBuffer *buffer = (StringBuffer*)connection->data;
+		Connection_send(connection, buffer->string->value, buffer->string->length);
+		break;
 	}
-}
-
-static void
-listen_to_client(ev_loop *loop, int fd)
-{
-	unblock(fd);
-	ev_io *client_data_w = malloc(sizeof(ev_io));
-	ev_io_init(client_data_w, on_client_read, fd, EV_READ);
-	ev_io_start(loop, client_data_w);
-}
-
-// all watcher callbacks have a similar signature
-// this callback is called when data is readable on stdin
-static void
-on_connection(ev_loop *loop, ev_io *w, int revents)
-{
-	Worker *worker = ev_userdata(loop);
-
-//	ev_io_stop(loop, connection_watcher);
-	XFLOG("Worker[%lu] fd:%d - new connection (revents:%d)\n",
-	      worker->id, w->fd, revents);
-
-	int client_fd = accept(server_fd, NULL, NULL);
-	if (client_fd == ACCEPT_ERROR)
+	case CONNECTION_EVENT_CLOSE_READ:
 	{
-		XFLOG("Worker[%lu] fd:%d - failed to accept\n", worker->id, w->fd);
-		return;
+		XDBG("close read");
+		const char *byebye = "bye bye\n";
+		Connection_send(connection, byebye, strlen(byebye));
+		break;
 	}
-	else
-	{
-		enable_so_opt(client_fd, SO_NOSIGPIPE); /* do not send SIGIPIPE on EPIPE */
-		XFLOG("Worker[%lu] fd:%d - accepted connection\n", worker->id, w->fd);
-		// TODO start data watcher here
-
-		listen_to_client(loop, client_fd);
+	case CONNECTION_EVENT_RECEIVE_TIMEOUT:
+	case CONNECTION_EVENT_ERRNO:
+	case CONNECTION_EVENT_ERROR_WRITE:
+		break;
 	}
-}
-
-/* connection timeout */
-static void
-timeout_cb(ev_loop *loop, ev_timer *w, int revents)
-{
-	printf("At your service!\n");
-	// this causes the innermost ev_run to stop iterating
-//	ev_break(loop, EVBREAK_ALL);
+	return connection;
 }
 
 int
-main(void)
+main(int argc, char** argv)
 {
-	ev_io connection_watcher;
-	ev_timer timeout_watcher;
-	// use the default event loop unless you have special needs
-	ev_loop *loop = EV_DEFAULT;
+	Server *server = (Server*)UnixServer_new("/tmp/echo.sock");
 
-#if EV_COMPAT3
-	puts("EV_COMPAT3 is set");
-#endif
-
-	server_fd = unix_socket_connect("/tmp/echo.sock");
-	if (server_fd == SOCKET_CONNECT_FAILED)
-	{
-		fprintf(stderr, "Error on socket connection. Exit now\n");
-		exit(1);
-	}
-
-	/* get notified on incomming connections */
-	ev_io_init(&connection_watcher, on_connection, server_fd, EV_READ);
-	ev_io_start(loop, &connection_watcher);
-
-	/* simple timer, repeating every 5 seconds */
-	ev_timer_init(&timeout_watcher, timeout_cb, 0., 5.);
-	ev_timer_again(loop, &timeout_watcher);
-
-	ev_run(loop, 0);
-
-	return 0;
+	server->worker_count = 4;
+	server->f_connection_handler = echo_connection_handler;
+	server->f_connection_data_handler = echo_connection_data_handler;
+	int ret = Server_start(server); // blocks
 }
