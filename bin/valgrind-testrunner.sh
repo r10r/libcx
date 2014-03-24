ce16c4783918aa726648d00f7c36e16362d382b7
Ruben Jenster <rjenster@gmail.com>
2014-03-24 13:21:09 +0100

    Fixed valgrind test runner (exit code / posix compliance)

--- bin/valgrind-testrunner.sh
+++ bin/valgrind-testrunner.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # On success the test launcher creates the $TESTRESULT_FILE
 # This is required for make to detect test failures.
 
@@ -9,11 +10,8 @@ VALGRIND_OPTIONS="--dsymutil=yes --fullpath-after=."
 # --dsymutil=yes is required to get line numbers (only on OSX ?)
 # see http://stackoverflow.com/questions/1221833/bash-pipe-output-and-capture-exit-status
 valgrind -q --error-exitcode=1  ${VALGRIND_OPTIONS} \
-	--tool=memcheck --leak-check=full ${TEST_BIN} 2>&1 \
-	| tee ${TEST_BIN}.testlog; test ${PIPESTATUS[0]} -eq 0
+	--tool=memcheck --leak-check=full ${TEST_BIN} 2>&1
 
 RESULT=$?
-
 [ $RESULT -eq 0 ] && touch $TESTRESULT_FILE
-
 exit $RESULT
