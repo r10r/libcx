b5d4e421692a6c50b042509d90f2ec2515c0ecf5
Ruben Jenster <rjenster@gmail.com>
2014-03-25 16:19:20 +0100

    Moved queue sources from libcx-list. Implement Qeue_destroy/Queue_active

---
+++ queue.c
@@ -0,0 +1,108 @@
+#include "queue.h"
+
+Queue*
+Queue_new()
+{
+	Queue *queue = malloc(sizeof(Queue));
+
+	queue->mutex_add_item = malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_init(queue->mutex_add_item, NULL);
+
+	queue->mutex_cond_add_item = malloc(sizeof(pthread_cond_t));
+	pthread_cond_init(queue->mutex_cond_add_item, NULL);
+
+	queue->items = List_new();
+
+	queue->_active = true;
+
+	return queue;
+}
+
+void
+Queue_free(Queue *queue)
+{
+	List_free(queue->items);
+
+	pthread_mutex_destroy(queue->mutex_add_item);
+	free(queue->mutex_add_item);
+
+	pthread_cond_destroy(queue->mutex_cond_add_item);
+	free(queue->mutex_cond_add_item);
+
+	free(queue);
+}
+
+// POP returns NULL when Queue is not active anymore ?
+// -> check for null in the calling thread
+void*
+Queue_pop(Queue *queue)
+{
+	void *data = NULL;
+	int rc = 0;
+
+	rc = pthread_mutex_lock(queue->mutex_add_item);
+	XFCHECK(rc == 0,
+		"pthread_mutex_lock should exit with 0 (was %d)", rc);
+
+	while (Queue_active(queue) && queue->items->length == 0)
+	{
+		rc = pthread_cond_wait(queue->mutex_cond_add_item, queue->mutex_add_item);
+		XFCHECK(rc == 0,
+			"pthread_cond_wait should exit with 0 (was %d)", rc);
+	}
+
+	/*
+	 * When the queue was destroyed/deactivated, multiple threads
+	 * access this region concurrently. To ensure
+	 * that only a single thread calls List_pop we have
+	 * to check whether the queue is active.
+	 */
+	if (Queue_active(queue))
+		data = List_pop(queue->items);
+
+	pthread_mutex_unlock(queue->mutex_add_item);
+	XFCHECK(rc == 0,
+		"pthread_mutex_unlock should exit with 0 (was %d)", rc);
+	return data;
+}
+
+int
+Queue_add(Queue *queue, void *data)
+{
+	if (!Queue_active(queue))
+		return -1;
+
+	int rc = 0;
+
+	// sender (task was added to queue)
+	pthread_mutex_lock(queue->mutex_add_item);
+	XFCHECK(rc == 0,
+		"pthread_mutex_lock should exit 0 (was %d)", rc);
+
+	List_unshift(queue->items, data);
+	// wake up a single thread that is waiting for the condition
+	int ret = pthread_cond_signal(queue->mutex_cond_add_item);
+	XFCHECK(rc == 0,
+		"pthread_cond_signal should exit with 0 (was %d)", rc);
+
+	pthread_mutex_unlock(queue->mutex_add_item);
+	XFCHECK(rc == 0,
+		"pthread_mutex_unlock should exit 0 (was %d)", rc);
+
+	return rc;
+}
+
+void
+Queue_destroy(Queue *queue)
+{
+	int rc = 0;
+
+	printf("Queue is destroyed\n");
+
+	queue->_active = false;
+
+	/* wakeup up all waiting threads */
+	rc = pthread_cond_broadcast(queue->mutex_cond_add_item);
+}
+
+
---
+++ test_queue.c
@@ -0,0 +1,138 @@
+#include "libcx-base/test.h"
+#include "queue.h"
+#include <unistd.h>
+#ifdef __linux__
+#include <signal.h>
+#endif
+
+/*
+ * Observations:
+ * 90% of the time is spend in for acquiring and releasing the lock
+ * as well as signaling / waiting for the condition
+ *
+ */
+NOSETUP
+PROFILE_INIT
+
+#define NTHREADS 4
+#define NITERATATIONS 100000
+
+typedef struct consumer_t
+{
+	Queue *queue;
+	int id;
+	pthread_t *thread;
+	int processed;
+} Consumer;
+
+static Consumer *
+Consumer_new(Queue *queue, int id)
+{
+	Consumer *consumer = malloc(sizeof(Consumer));
+
+	consumer->id = id;
+	consumer->queue = queue;
+	consumer->thread = malloc(sizeof(pthread_t));
+	consumer->processed = 0;
+	return consumer;
+}
+
+static void
+Consumer_free(Consumer *consumer)
+{
+	free(consumer->thread);
+	free(consumer);
+}
+
+static void*
+start_consumer(void *data)
+{
+	Consumer *consumer = (Consumer*)data;
+
+	while (Queue_active(consumer->queue))
+	{
+		int *x = Queue_pop(consumer->queue);
+
+		// last iteration might return NULL
+		if (x)
+		{
+			int i = *x;
+			free(x);
+			consumer->processed++;
+
+			// queue is destroyed on the last request
+			if (i == (NITERATATIONS - 1))
+			{
+				printf("Consumer[%d] I'm processing the last request\n", consumer->id);
+				Queue_destroy(consumer->queue);
+			}
+		}
+	}
+	printf("Consumer[%d] Leaving inactive queue\n", consumer->id);
+	return NULL;
+}
+
+static Consumer *
+Consumer_start(Queue *queue, int id)
+{
+	Consumer *consumer = Consumer_new(queue, id);
+
+	pthread_create(consumer->thread, NULL, start_consumer, consumer);
+	return consumer;
+}
+
+static void
+test_Queue()
+{
+	Queue *queue = Queue_new();
+
+	Consumer* consumers[NTHREADS];
+
+	int i_thread;
+
+	for (i_thread = 0; i_thread < NTHREADS; i_thread++)
+		consumers[i_thread] = Consumer_start(queue, i_thread);
+
+
+	PROFILE_BEGIN_FMT("Processing %d simple requests with %d threads\n",
+			  NITERATATIONS, NTHREADS);
+
+	int i_item;
+	int expected_sum = 0;
+	for (i_item = 0; i_item < NITERATATIONS; i_item++)
+	{
+		int *x = malloc(sizeof(int));
+		*x = i_item;
+		Queue_add(queue, x);
+		expected_sum += i_item;
+	}
+
+	// wake up all threads and tell them to finish up
+	int total_processed = 0;
+	for (i_thread = 0; i_thread < NTHREADS; i_thread++)
+	{
+		Consumer *consumer = consumers[i_thread];
+		// see http://stackoverflow.com/questions/5610677/valgrind-memory-leak-errors-when-using-pthread-create
+		//	http://stackoverflow.com/questions/5282099/signal-handling-in-pthreads
+		pthread_join(*consumer->thread, NULL);
+		printf("Consumer[%d] processed %d\n", consumer->id, consumer->processed);
+		total_processed += consumer->processed;
+		Consumer_free(consumer);
+	}
+
+	PROFILE_END
+
+	TEST_ASSERT_EQUAL(total_processed, NITERATATIONS);
+
+	Queue_free(queue);
+}
+
+int
+main()
+{
+	TEST_BEGIN
+
+	RUN(test_Queue);
+
+	TEST_END
+}
