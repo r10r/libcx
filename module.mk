d15abbee733ec5e8b3cd7e9b7b057e01c2329703
Ruben Jenster <rjenster@gmail.com>
2014-03-25 16:20:07 +0100

    Moved queue sources to libcx-socket-unix.

--- module.mk
+++ module.mk
@@ -3,19 +3,17 @@ L := $(LOCAL_DIR)
 
 #CFLAGS += -D_LIST_DISABLE_LOCKING
 
-TESTS += $(L)/test_list $(L)/test_array $(L)/test_queue
+TESTS += $(L)/test_list \
+	$(L)/test_array \
+
 
 # -- executables --
 #_OBJS := 
 #_FLAGS := 
 
 # -- tests -- 
-$(L)/test_list_OBJS := $(TEST_OBJS) $(L)/test_list.o $(L)/list.o
-$(L)/test_list_FLAGS := $(TEST_FLAGS)
-
-$(L)/test_array_OBJS := $(TEST_OBJS) $(L)/test_array.o $(L)/array.o
-$(L)/test_array_FLAGS := $(TEST_FLAGS)
-
-$(L)/test_queue_OBJS := $(TEST_OBJS) $(L)/test_queue.o $(L)/queue.o \
+$(L)/test_list_OBJS := $(TEST_OBJS) $(L)/test_list.o \
 	$(L)/list.o
-$(L)/test_queue_FLAGS := $(TEST_FLAGS)
+
+$(L)/test_array_OBJS := $(TEST_OBJS) $(L)/test_array.o \
+	$(L)/array.o
