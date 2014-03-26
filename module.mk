53c746b01d8f2596a78c9112672e386e7a5fa3b1
Ruben Jenster <rjenster@gmail.com>
2014-03-26 13:53:57 +0100

    Cleanup module Makefiles.

--- module.mk
+++ module.mk
@@ -8,8 +8,8 @@ TESTS += $(L)/test_debug $(L)/test_base
 #_FLAGS := 
 
 # -- tests -- 
-$(L)/test_debug_OBJS := $(TEST_OBJS) $(L)/test_debug.o
-$(L)/test_debug_FLAGS := $(TEST_FLAGS)
+$(L)/test_debug_OBJS := $(TEST_OBJS) \
+	$(L)/test_debug.o
 
-$(L)/test_base_OBJS := $(TEST_OBJS) $(L)/test_base.o
-$(L)/test_base_FLAGS := $(TEST_FLAGS)
+$(L)/test_base_OBJS := $(TEST_OBJS) \
+	$(L)/test_base.o
