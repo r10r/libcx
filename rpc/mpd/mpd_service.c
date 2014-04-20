ded3e385b9eeb97f3b167602ba476d37d97ce67f
Ruben Jenster <rjenster@gmail.com>
2014-04-20 14:45:09 +0200

    Added MPD service implementation for play/pause/send_message methods.

--- rpc/mpd/mpd_service.c
+++ rpc/mpd/mpd_service.c
@@ -1,32 +1,75 @@
 #include "mpd_service.h"
 
-static void
-play(const char* track)
+static struct mpd_connection* mpd_connection = NULL;
+
+static int
+connect(struct mpd_connection** conn)
 {
-	printf("Play %s\n", track);
+	if (*conn == NULL)
+	{
+		*conn = mpd_connection_new(NULL, 0, 30000);
+		if (*conn == NULL)
+		{
+			fprintf(stderr, "%s\n", "Out of memory");
+			return -1;
+		}
+
+		if (mpd_connection_get_error(*conn) != MPD_ERROR_SUCCESS)
+		{
+			fprintf(stderr, "Failed to create connection: %s\n", mpd_connection_get_error_message(*conn));
+			mpd_connection_free(*conn);
+			*conn = NULL;
+			return -1;
+		}
+	}
+	return 1;
 }
 
 static void
-pause(const char* track)
+mpd_clear_error()
 {
-	printf("Pause\n");
+	mpd_response_finish(mpd_connection);
+	if (mpd_connection_get_error(mpd_connection) != MPD_ERROR_SUCCESS)
+	{
+		fprintf(stderr, "Connection error: %s\n", mpd_connection_get_error_message(mpd_connection));
+		mpd_connection_clear_error(mpd_connection);
+	}
 }
 
-/* service methods */
-
-RPC(params, play)
-{
-	{ "track", get_param_value_string, 0 }
-};
 RPC(method, play)
 {
-	play((char*)request->params[0]);
+	if (connect(&mpd_connection) == 1)
+	{
+		bool playing = mpd_send_play(mpd_connection);
+		printf("Play %d\n", playing);
+		mpd_clear_error();
+	}
 }
 
 RPC(method, pause)
 {
-	pause((char*)request->params[0]);
+	if (connect(&mpd_connection) == 1)
+	{
+		bool paused = mpd_send_pause(mpd_connection, 0);
+		printf("Paused %d\n", paused);
+		mpd_clear_error();
+	}
 }
 
-RPC(export, play);
+RPC(params, send_message)
+{
+	{ "message", get_param_value_string, 0 }
+};
+RPC(method, send_message)
+{
+	if (connect(&mpd_connection) == 1)
+	{
+		bool paused = mpd_run_send_message(mpd_connection, "foo", (const char*)request->params[0]);
+		printf("Message send %d\n", paused);
+		mpd_clear_error();
+	}
+}
+
+RPC(export_without_params, play);
 RPC(export_without_params, pause);
+RPC(export, send_message);
--- rpc/mpd/mpd_service.h
+++ rpc/mpd/mpd_service.h
@@ -3,6 +3,16 @@
 
 #include "rpc/jsrpc.h"
 
+#include <mpd/client.h>
+#include <mpd/status.h>
+#include <mpd/song.h>
+#include <mpd/entity.h>
+#include <mpd/search.h>
+#include <mpd/tag.h>
+#include <mpd/idle.h>
+
+#include <mpd/connection.h>
+
 /* namespace  declaration */
 #undef RPC
 #define RPC(action, cmd) RPC_ ## action(MusicPlayerDaemon, cmd)
@@ -10,9 +20,11 @@
 /* export each function + method definition */
 RPC(public, play)
 RPC(public, pause)
+RPC(public, send_message)
 
 #define MusicPlayerDaemon_methods \
 	RPC(public_name, play), \
-	RPC(public_name, pause)
+	RPC(public_name, pause), \
+	RPC(public_name, send_message)
 
 #endif
