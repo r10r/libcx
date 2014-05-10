dbbcaf626e9b1fa7355a832482e561be2147d578
Ruben Jenster <rjenster@gmail.com>
2014-05-10 18:25:11 +0200

    Removed newlines from JSONRPC format strings. Added macros JSRPC_STRINGPAIR, JSRPC_OBJECT.

--- rpc/test_jsonrpc.c
+++ rpc/test_jsonrpc.c
@@ -4,11 +4,29 @@
 static void
 test_jsrpc_printf()
 {
-	printf("%s", JSONRPC_RESPONSE);
-	printf("%s", JSONRPC_RESPONSE_STRING);
-	printf("%s", JSONRPC_ERROR);
+	printf("%s\n", JSONRPC_RESPONSE);
+	printf("%s\n", JSONRPC_RESPONSE_STRING);
+	printf("%s\n", JSONRPC_ERROR);
 	jsrpc_fprintf_response(stdout, "22", "foobar");
 	jsrpc_fprintf_error(stdout, NULL, jsrpc_ERROR_INVALID_REQUEST, "Invalid request");
+}
+
+static void
+test_write_object()
+{
+	StringBuffer* buffer = StringBuffer_new(1024);
+
+	// \0 terminator belongs to string length or not ?
+	// are strings always null terminated or not
+
+	StringBuffer_cat(buffer, "{");                                  /* add null terminator, but does not overwrite it */
+	StringBuffer_aprintf(buffer, JSRPC_STRINGPAIR("foo"), "bar");   /* adds null terminator, overwrites null terminator */
+	StringBuffer_cat(buffer, "}");
+
+	printf("%s\n", StringBuffer_value(buffer));
+	TEST_ASSERT_EQUAL_STRING("{\"foo\":\"bar\"}", StringBuffer_value(buffer));
+
+	StringBuffer_free(buffer);
 }
 
 int
@@ -17,6 +35,7 @@ main()
 	TEST_BEGIN
 
 	RUN(test_jsrpc_printf);
+	RUN(test_write_object);
 
 	TEST_END
 }
