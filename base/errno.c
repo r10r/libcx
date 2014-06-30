13bcdf83fa1470bf2e2f8c37c26308f2ba97bb24
Ruben Jenster <rjenster@gmail.com>
2014-06-30 16:32:29 +0200

    Added error tracking. (print debug information when calling set_cx_error)

--- base/errno.c
+++ base/errno.c
@@ -3,3 +3,14 @@
 // http://programmers.stackexchange.com/questions/209729/rationale-behind-c-library-functions-never-setting-errno-to-zero
 
 __thread int cx_errno = 0;
+
+void
+dbg_set_cx_errno(int err, const char* file, int line, const char* func)
+{
+	if (err != 0)
+	{
+		flockfile(stderr);
+		fprintf(stderr, "CX_ERRNO %d - %s %d (%s)\n", err, file, line, func);
+	}
+	cx_errno = err;
+}
--- base/errno.h
+++ base/errno.h
@@ -1,8 +1,19 @@
 #ifndef _CX_ERRNO_H
 #define _CX_ERRNO_H
 
+#include <stdio.h>
+
 extern __thread int cx_errno;
+
+void
+dbg_set_cx_errno(int err, const char* file, int line, const char* func);
+
+#ifdef _CX_DEBUG
+#define set_cx_errno(err) (dbg_set_cx_errno(err, __FILE__, __LINE__, __func__))
+#else
 #define set_cx_errno(err) (cx_errno = (err))
+#endif
+
 //#define cx_errno (cx_errno)
 
 #endif
